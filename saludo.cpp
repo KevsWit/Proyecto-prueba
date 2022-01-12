8b65ed857b0c7c7676186e55a2d204b15c6426be
Kevin Castillo <kcastilloo1@est.ups.edu.ec>
2022-01-11 22:33:30 -0500

    Agregado dato al proyecto

--- saludo.cpp
+++ saludo.cpp
@@ -3,6 +3,7 @@
 int main(){
 	std::cout << "Hello World!\n";
 	std::cout << "My name is Kevin." << std::endl;
+	std::cout << "I am 20 years." << std::endl;
 	std::cout << "Today is a good day!";
 	std::cout << std::endl;
 	return 0;
